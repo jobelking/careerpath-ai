0236b2737e747b138de00360657bbdab9e5a9ee3
jobelking <jobelkingbautista5@gmail.com>
2026-03-05 22:58:06 +0800

    icons

---
+++ frontend/src/utils/authIcons.js
@@ -0,0 +1,15 @@
+import {
+    FaUser,
+    FaEnvelope,
+    FaLock,
+    FaTimes,
+    FaSignOutAlt
+} from 'react-icons/fa';
+
+export const authIcons = {
+    FaUser,
+    FaEnvelope,
+    FaLock,
+    FaTimes,
+    FaSignOutAlt
+};
